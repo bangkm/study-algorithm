585d4e533bb391eaceff8381c195d92854b102d0
bangkm <kmbang04@gmail.com>
2019-10-01 16:36:16 +0900

    01, Oct, 2019 - Algorithm Study (BaekJoon) - ATM(#11399) - Greedy Algorithm

---
+++ baekjoon/greedy_algorithm/atm/Solution.py
@@ -0,0 +1,12 @@
+n = int(input())
+times = list(map(int, input().split()))
+
+times.sort()
+sumList = 0
+result = []
+
+for time in times:
+    sumList += time
+    result.append(sumList)
+
+print(sum(result))
